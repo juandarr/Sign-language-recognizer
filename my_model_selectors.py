c42a7f292f265c5ae2e2c3d8579b48a9b4f43cfc
Juan Rios <juandarr@gmail.com>
2019-04-08 02:21:48 -0500

    init: initial commit with files from already completed project.

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -7,7 +7,6 @@ from hmmlearn.hmm import GaussianHMM
 from sklearn.model_selection import KFold
 from asl_utils import combine_sequences
 
-
 class ModelSelector(object):
     '''
     base class for model selection (strategy design pattern)
@@ -30,6 +29,7 @@ class ModelSelector(object):
 
     def select(self):
         raise NotImplementedError
+
 
     def base_model(self, num_states):
         # with warnings.catch_warnings():
@@ -62,7 +62,7 @@ class SelectorConstant(ModelSelector):
 
 
 class SelectorBIC(ModelSelector):
-    """ select the model with the lowest Bayesian Information Criterion(BIC) score
+    """ select the model with the lowest Baysian Information Criterion(BIC) score
 
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
@@ -76,9 +76,37 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        #implement model selection based on BIC scores
 
+        try:
+            # Initializes best_score with the minimum value possible
+            best_score = float("-inf")
+            # Creates models with number of states between self.min_n_components and self.max_n_components
+            for i in range(self.min_n_components, self.max_n_components+1):
+                try:
+                    # Creates the model and defines BIC score
+                    hmm_model = GaussianHMM(n_components=i, covariance_type="diag", n_iter=1000,
+                                                random_state=self.random_state, verbose=False).fit(self.X,
+                                                                                                   self.lengths)
+                    #Calculates number of parameters
+                    parameters = i*i + 2*i*len(self.X[0]) -1
+                    #Calculates BIC score
+                    score = (-2)*hmm_model.score(self.X, self.lengths)+parameters*np.log(len(self.X))
+                except:
+                    continue
+                # If the score is better than the current one store the score and the number of states
+                if best_score < score:
+                    best_score = score
+                    best_n_components = i
+
+            if self.verbose:
+                print("model selected for {}".format(self.this_word))
+            return self.base_model(best_n_components)
+
+        except:
+            if self.verbose:
+                print("failure on {}".format(self.this_word))
+            return None
 
 class SelectorDIC(ModelSelector):
     ''' select best model based on Discriminative Information Criterion
@@ -86,15 +114,56 @@ class SelectorDIC(ModelSelector):
     Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
-    https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # implement model selection based on DIC scores
+
+        try:
+            # Initializes best_score with the minimum value possible
+            best_score = float("-inf")
+            # Creates models with number of states between self.min_n_components and self.max_n_components
+            for i in range(self.min_n_components, self.max_n_components+1):
+                try:
+                    # Creates the model and initializes values to calculate DIC score
+                    hmm_model = GaussianHMM(n_components=i, covariance_type="diag", n_iter=1000,
+                                            random_state=self.random_state, verbose=False).fit(self.X,
+                                                                                               self.lengths)
+                    likeLog = hmm_model.score(self.X, self.lengths)
+                    antilikeLog = 0
+                    total_scores = 0
+                    #Stores all likelihoods for the other words in the set of words
+                    for word in self.words:
+                        if (word== self.this_word):
+                            continue
+                        X, length = self.hwords[word]
+                        try:
+                            antilikeLog += hmm_model.score(X,length)
+                            total_scores+= 1
+                        except:
+                            continue
+                    #Normalize log likelihood value
+                    antilikeLog /= total_scores
+                    #Define the score as the different between the log likelihood of the data of the words and the average of the sum of likelihoods for the rest of the words
+                    score = likeLog - antilikeLog
+                except:
+                    continue
+                # If the score is better than the current one store the score and the number of states
+                if best_score < score:
+                    best_score = score
+                    best_n_components = i
+
+            if self.verbose:
+                print("model selected for {}".format(self.this_word))
+            return self.base_model(best_n_components)
+
+        except:
+            if self.verbose:
+                print("failure on {}".format(self.this_word))
+            return None
 
 
 class SelectorCV(ModelSelector):
@@ -105,5 +174,42 @@ class SelectorCV(ModelSelector):
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        try:
+            #Initializes best_score with the minimum value possible
+            best_score = float("-inf")
+            #Creates models with number of states between self.min_n_components and self.max_n_components
+            for i in range(self.min_n_components, self.max_n_components+1):
+                #Splits self.sequences in folds
+                split_method = KFold(n_splits=min(3,len(self.sequences)))
+                #List to store the log likelihood of all the KFolds of a model for a given state
+                logList =[]
+                for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                    #Combine sequences to form training data
+                    training_data,length_train = combine_sequences(cv_train_idx, self.sequences)
+                    #Combine the sequences to form the testing data
+                    test_data, length_test = combine_sequences(cv_test_idx, self.sequences)
+                    try:
+                        #Creates the model and append the score (log likelihood) associated to the specific KFold element
+                        hmm_model = GaussianHMM(n_components=i, covariance_type="diag", n_iter=1000,random_state=self.random_state, verbose=False).fit(training_data, length_train)
+                        logList.append(hmm_model.score(test_data, length_test))
+                    except:
+                        continue
+                if (len(logList)>0):
+                    #Take the average of the scores for all the possible separation of sequences given by KFold
+                    logL_avg = sum(logList)/len(logList)
+                    #If the score is better than the current one store the score and the number of states
+                    if best_score < logL_avg:
+                        best_score = logL_avg
+                        best_n_components = i
+                else:
+                    continue
+
+            if self.verbose:
+                print("model created for {}".format(self.this_word))
+            return self.base_model(best_n_components)
+
+        except:
+            if self.verbose:
+                print("failure on {}".format(self.this_word))
+            return None
+
--- my_recognizer.py
+++ my_recognizer.py
@@ -4,7 +4,6 @@ from asl_data import SinglesData
 
 def recognize(models: dict, test_set: SinglesData):
     """ Recognize test word sequences from word models set
-
    :param models: dict of trained models
        {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
    :param test_set: SinglesData object
@@ -20,6 +19,23 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    #implement the recognizer
+    # Explore all the test sequences
+    for X, length in test_set.get_all_Xlengths().values():
+        # Dictionary to store the log prob for all the word models tested with the current set of frames (X,length)
+        dict_prob = {}
+        # Explore all the words of the database
+        for word, word_model in models.items():
+            try:
+                dict_prob[word] = word_model.score(X,length)
+            except:
+                dict_prob[word] = float("-inf")
+                continue
+        # Append dictionary to the list of probabilities for the sets of testing frames
+        probabilities.append(dict_prob)
+
+        # Store dict in tuples with value as the first element
+        inverse = [(value,key) for key,value in dict_prob.items()]
+        # The best guess is the tuple having the highest value
+        guesses.append(max(inverse)[1])
+    return probabilities, guesses
